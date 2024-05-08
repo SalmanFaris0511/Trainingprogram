7c4d09791b11b3517ef7bd8210e2e7f99beebc53
salmanfaris0511 <farissalman0511@gmail.com>
2024-05-08 10:27:57 +0530

    find max added

--- find max.py
+++ find max.py
@@ -1,1 +1,11 @@
-# Find a maximum of two numbers
+'''# Find a maximum of two numbers
+n=int(input("Enter a Number: "))
+s=int(input("Enter a Number: "))
+
+if n>s :
+    print(n, "is a maximum number")
+else:
+    print(s, " is a maximum number")  '''
+
+
+
